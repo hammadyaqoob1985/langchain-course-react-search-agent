883218820950ab5182859daf9c106d3f87e31728
hammadyaqoob1985 <hammad.yaqoob1985@yahoo.co.uk>
2025-12-23 19:11:07 +0000

    used with structure output instead of prompt injection and parsing

--- main.py
+++ main.py
@@ -9,29 +9,27 @@ from langchain_classic.agents import AgentExecutor
 from langchain_classic.agents import create_react_agent
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
-from langchain_core.output_parsers.pydantic import  PydanticOutputParser
+
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnableLambda
 
 from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
 from schemas import AgentResponse
 
-output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
-react_prompt_with_format_instructions = PromptTemplate(
-    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
-    input_variables=["input", "agent_scratchpad", "tool_names", "tools"]
-).partial(format_instructions=output_parser.get_format_instructions())
+
 
 tools = [TavilySearch()]
 llm = ChatOpenAI(model="gpt-4")
+structured_llm = llm.with_structured_output(AgentResponse)
 react_prompt = hub.pull("hwchase17/react")
-agent = create_react_agent(llm, tools, prompt=react_prompt_with_format_instructions)
+
+agent = create_react_agent(llm, tools, prompt=react_prompt)
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
 extract_output = RunnableLambda(lambda x: x["output"])
-parse_output = RunnableLambda(lambda x: output_parser.parse(x))
 
-chain = agent_executor | extract_output | parse_output
+
+chain = agent_executor | extract_output | structured_llm
 
 def main():
     print("Hello from langchain-course-react-search-agent!")
